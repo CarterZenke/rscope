0fce1c9dfdffadf3846a4a31208e47878f56d6f9
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2023-05-12 10:27:22 -0400

    improve, document `find_and_parse_lateness`

--- R/find_and_parse_lateness.R
+++ R/find_and_parse_lateness.R
@@ -1,10 +1,26 @@
+#' Find and Parse HH:MM:SS lateness columns in a Gradescope gradebook
+#'
+#' @param df The dataframe (gradebook)
+#' @param regex The regular expression with which to match the titles of lateness columns
+#' @param unit The unit to which to parse HH:MM:SS, in "seconds", "minutes", or "hours"
+#'
+#' @return Returns a dataframe with lateness columns parsed to specified unit
+#' @export
+#'
+#' @examples
+#' gradebook <- gradebook |> find_and_parse_lateness()
+#' gradebook <- gradebook |> find_and_parse_lateness(unit = "minutes")
+#' gradebook <- gradebook |> find_and_parse_lateness(unit = "hours")
+#' gradebook <- gradebook |> find_and_parse_lateness(regex = "late_work", unit = "hours")
 find_and_parse_lateness <- function(df, regex="- Lateness", unit="seconds") {
+
+  # Apply `parse_lateness`, row-wise, to each column that matches specified regular expression
   new <- df %>%
            dplyr::rowwise() %>%
            dplyr::mutate(
              dplyr::across(
                dplyr::matches(regex),
-               rscope::parse_lateness
+               ~ rscope::parse_lateness(., unit=unit)
              )
            )
 
